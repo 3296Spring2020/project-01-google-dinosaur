b086aad44efba2967442aa2b953f9d5a0047f61f
Tuyen Pham <tuh39709@temple.edu>
2020-03-30 21:06:17 -0400

    add the limited max score

--- scripts/runner.js
+++ scripts/runner.js
@@ -730,6 +730,7 @@
             });
 
             scoreboard.sort(function(a, b) { return a.Score > b.Score ? -1 : 1 });
+            var scoreboard = scoreboard.slice(0, 10);
             localStorage.SCOREBOARD = JSON.stringify(scoreboard);
         },
 
